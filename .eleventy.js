9db476b5eeb1645a9d48bfa20e61a931c65e3f5b
Mercy Birungi <mercyssbirungi@gmail.com>
2023-12-15 17:31:42 +0300

    my portfolio code

---
+++ .eleventy.js
@@ -0,0 +1,63 @@
+const { DateTime } = require("luxon")
+const pluginPWA = require("eleventy-plugin-pwa")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const criticalCss = require("eleventy-critical-css");
+const util = require('util');
+
+
+module.exports = function(eleventyConfig) {
+
+  // Add a filter using the Config API
+  eleventyConfig.addFilter( "postDate", function(dateObj) {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)    
+  });
+
+  // Add a filter for cleaning work url
+  eleventyConfig.addFilter( "urlClean", function(url) {
+    return url.split("//")[1]
+  });
+
+  //  check if isArticlePage
+  eleventyConfig.addFilter( "isArticlePage", function(url) {
+    return url.split("/").includes('blog')
+  });
+  
+  // debug filter 
+  eleventyConfig.addFilter('console', function(value) {
+      return util.inspect(value);
+  });
+
+  //add plugin
+  eleventyConfig.addPlugin(pluginPWA);
+  eleventyConfig.addPlugin(syntaxHighlight);
+  // eleventyConfig.addPlugin(criticalCss);
+
+  // adding config for tags collection
+  // eleventyConfig.addCollection("tagsList", function(collectionApi) {
+  //     const tagsList = new Set();
+  //     collectionApi.getAll().map( item => {
+  //         if (item.data.tags) { // handle pages that don't have tags
+  //             item.data.tags.map( tag => tagsList.add(tag))
+  //         }
+  //     });
+  //     return tagsList;
+  // });
+
+
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      input: "src",
+      output:"public",
+      includes: "_includes", // Add this line
+
+    }
+  };
+};
---
+++ README.md
@@ -0,0 +1,42 @@
+# Viewing an Eleventy Site Locally
+
+
+1. **Open Terminal or Command Prompt:**
+   - If you closed the terminal or command prompt, reopen it.
+
+2. **Navigate to the Cloned Eleventy Site Directory:**
+   - Use the `cd` command to enter the folder where you cloned the project. For example:
+     ```bash
+     cd path/to/your/folder/portfolio
+     ```
+
+3. **Install Dependencies:**
+   - Run the following command to install any required dependencies (if there is a `package.json` file):
+     ```bash
+     npm install
+     ```
+
+4. **Ensure Node.js Version 14:**
+   - Confirm that you have Node.js version 14 installed by running:
+     ```bash
+     node -v
+     ```
+     If you don't have version 14, you can download it from [Node.js](https://nodejs.org/).
+
+5. **Build the Eleventy Site:**
+   - Run the following command to build the Eleventy site:
+     ```bash
+     npx eleventy
+     ```
+
+6. **Start a Local Server:**
+ 
+     ```bash
+     npx eleventy --serve
+     ```
+
+7. **View the Site in Your Browser:**
+   - Open your web browser and go to the local address provided in the terminal. It's often `http://localhost:8080/`.
+
+
+# MyNewPortfolio
